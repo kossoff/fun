d665f5c810aa6c4e3f1edb3ca6e849448f344f5a
Anthony K. @Big Mac <off.koss@gmail.com>
2015-12-01 22:52:25 +0300

    First styles ;)

--- template.php
+++ template.php
@@ -17,3 +17,12 @@ function fun_preprocess_page(&$variables) {
  */
 function fun_preprocess_node(&$variables) {
 }
+
+function fun_links__topbar_main_menu($variables) {
+  // We need to fetch the links ourselves because we need the entire tree.
+  $links = menu_tree_output(menu_tree_all_data(variable_get('menu_main_links_source', 'main-menu')));
+  $output = _zurb_foundation_links($links);
+  // $variables['attributes']['class'][] = '';
+
+  return '<ul' . drupal_attributes($variables['attributes']) . '><li><a href="tel:+79852700557">+7 (985) 270-05-57</li>' . $output . '</ul>';
+}
