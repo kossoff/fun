535a8f2c63c41187d85275d606d778f1b8360484
Anthony K. @Big Mac <off.koss@gmail.com>
2016-02-25 16:37:34 +0300

    Add end to columns of event cards view

--- templates/views-view-unformatted--calendar-events.tpl.php
+++ templates/views-view-unformatted--calendar-events.tpl.php
@@ -12,7 +12,7 @@
 <?php endif; ?>
 <div class="row collapse" data-equalizer>
   <?php foreach ($rows as $id => $row): ?>
-    <div class="large-4 columns" <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <div class="large-4 columns end" <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
       <div class="wrapper" data-equalizer-watch>
         <?php print $row; ?>
       </div>
